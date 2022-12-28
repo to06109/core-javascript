202c5bec1fd0ddcb5b58157225e1df6c42eac747
to06109 <to06109@naver.com>
2022-12-28 15:28:03 +0900

    [추가] type conversion 학습

---
+++ client/chapter/core/06.typeConversion.js
@@ -0,0 +1,65 @@
+/* ---------------------------------------------------------------------- */
+/* Type Conversion                                                        */
+/* ---------------------------------------------------------------------- */
+
+/* 데이터 → 문자 ----------------------------------------------------------- */
+
+// String() 함수 이용
+// number -> '2022'
+const YEAR = 2022
+console.log(typeof String(YEAR))
+
+// undefined -> 'undefined'
+// null -> 'null'
+console.log(typeof String(undefined))
+console.log(typeof String(null))
+
+// boolean -> 'true' or 'false'
+console.log(typeof String(true))
+console.log(typeof String(false))
+
+// String함수로 형변환 하지 않아도 문자열로 바꿀 수 있음
+console.log(typeof (true + ''))
+
+/* 데이터 → 숫자 ----------------------------------------------------------- */
+
+// undeinfed
+console.log(Number(undefined)) // NaN
+
+// null
+console.log(Number(null)) // 0
+
+// boolean
+console.log(Number(true)) // 1
+console.log(Number(false)) // 0
+// Number 함수 없이 형변환
+console.log(true * 1) // 1
+console.log(true / 1) // 1
+console.log(false / 1) // 0
+console.log(+false) // 0
+
+// string 숫자형 문자
+let num = '   123' // 숫자형 문자인 경우, 공백을 제거하고 숫자로 반환
+console.log(num * 1) // 123
+console.log(Number('')) // 0
+console.log(Number('aa')) // NaN
+
+// numeric string
+let width = '320px'
+console.log(parseInt(width, 10)) // 320. 문자열에서 정수만 쏙 빼줌
+
+width = '320.123456789px'
+console.log(parseFloat(width, 10)) //320.123456789. 소수점까지 빼줌
+
+/* 데이터 → 불리언 ---------------------------------------------------------- */
+
+// null, undefined, 0, NaN, ''
+// 위에 나열한 것 이외의 것들
+
+console.log(Boolean(null)) //false
+console.log(Boolean(undefined)) //false
+console.log(Boolean(0)) //false
+console.log(Boolean('')) //false
+console.log(Boolean(' ')) //true
+// Boolean 함수 없이 형변환
+console.log(!!null) //false
