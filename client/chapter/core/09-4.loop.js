7624b4d41561c46987449b400c8f5b6683553333
to06109 <to06109@naver.com>
2023-01-03 02:44:40 +0900

    [추가] hasOwnProperty 학습

---
+++ client/chapter/core/09-4.loop.js
@@ -0,0 +1,44 @@
+/* ---------------------------------------------------------------------- */
+/* For In Loop                                                            */
+/* ---------------------------------------------------------------------- */
+
+const javaScript = {
+  creator: 'Brendan Eich',
+  createAt: '1995.05',
+  standardName: 'ECMAScript',
+  currentVersion: 2022,
+  // hasOwnProperty: function () {
+  //   return '헤헤'
+  // },
+}
+
+let key = 'standardName'
+
+console.log(javaScript)
+// key가 javaScript 객체에 들어있는가?
+console.log(key in javaScript) // true
+console.log('toString' in javaScript) // true
+console.log('hasOwnProperty' in javaScript) // true
+
+// 객체의 속성(property) 포함 여부 확인 방법
+// - 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
+
+// console.log(javaScript.hasOwnProperty(key)) // -> 이렇게하면 안됨
+// Object.prototype.nickName = 'tiger'
+
+// 객체 자신의(own) 속성(property)인지 확인(has)하는 방법
+// - "자신의 속성을 가지고있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
+
+console.log(Object.prototype.hasOwnProperty.call(javaScript, key)) // true
+
+let a = {}
+
+for (let key in javaScript) {
+  if ({}.hasOwnProperty.call(javaScript, key)) {
+    console.log(key)
+  }
+}
+
+// for ~ in 문
+// - 객체 자신의 속성만 순환하려면?
+// - 배열 객체 순환에 사용할 경우?
