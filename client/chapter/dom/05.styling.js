1fc459631976f397b5cd82692fd9299182aa321c
to06109 <to06109@naver.com>
2023-01-11 10:54:58 +0900

    [추가] getCss 함수

--- client/chapter/dom/05.styling.js
+++ client/chapter/dom/05.styling.js
@@ -17,16 +17,35 @@ first.classList.contains('hello') // true
 
 // 유틸함수 만들기 -> lib/dom/css.js로 이동
 addClass('.first', 'hello') // <span class="first hello">hello</span>
-// removeClass('.first', 'hello') // <span class="first">hello</span>
+removeClass('.first', 'hello') // <span class="first">hello</span>
 // removeClass('.first') // <span class>hello</span>
-toggleClass('.first', 'hi')
+// toggleClass('.first', 'hi')
 
-// toggleClass('.first', 'hello')
+toggleClass('.first', 'hello2')
 
 /* 스타일 변경 방법 --------------------------------------------------------- */
 
 // - style.cssText - "style" 속성 전체에 대응하므로 스타일 전체에 대한 문자열 저장
 
-/* 계산된 스타일 읽기 ------------------------------------------------------- */
+// first.style.transform = 'rotate(360deg)'
+first.style.backgroundColor = 'red'
 
+/* 계산된 스타일 읽기 ------------------------------------------------------- */
+let size = getComputedStyle(first).fontSize
+console.log(size) // 32px
 // - getComputedStyle(element, [pseudoElement]) `읽기 전용`
+
+// 자바스크립트에선 객체의 key, value값을 변수로 받기 위해서는 . 사용 x
+// [ ] 대활호 표기법
+function getCss(node, prop) {
+  if (typeof node === 'string') node = getNode(node)
+  if (!(prop in document.body.style)) {
+    syntaxError(
+      'getCss 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.',
+    )
+  }
+  return getComputedStyle(node)[prop]
+}
+
+// 정확한 css 속성인지?
+console.log(getCss('.first', 'fontsize'))
