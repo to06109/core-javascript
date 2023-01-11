3821a3d956c3e0b4dd66295a655180fe9994f9d3
to06109 <to06109@naver.com>
2023-01-11 16:01:37 +0900

    [추가] 이벤트 부하 제거: Throttle, Debounce

--- client/chapter/dom/06.event.js
+++ client/chapter/dom/06.event.js
@@ -11,6 +11,9 @@
 /* 이벤트 추가/제거 --------------------------------------------------------- */
 
 const first = getNode('.first')
+const second = getNode('.second')
+const ground = getNode('.ground')
+const ball = getNode('.ball')
 
 function handler() {
   console.log('hit ! ')
@@ -21,14 +24,12 @@ function handler() {
 
 first.addEventListener('click', handler)
 
-const second = getNode('.second')
-second.addEventListener('click', function () {
-  first.removeEventListener('click', handler)
-})
+// second.addEventListener('click', function () {
+//   first.removeEventListener('click', handler)
+// })
 
 // - addEventListener
 // - removeEventListener
-
 
 // bindEvent 유틸 함수 만들기(클로저 이용)
 // const off = bindEvent('.first', 'click', handler)
@@ -36,3 +37,57 @@ second.addEventListener('click', function () {
 // off 하나로 이벤트 지우는 함수 넣어줄 수 있음
 // bindEvent('.second', 'click', off)
 
+ground.addEventListener('click', function (e) {
+  console.log(e.offsetX, e.offsetY)
+  ball.style.transform = `translate(${e.offsetX - ball.offsetWidth / 2}px, ${
+    e.offsetY - ball.offsetHeight / 2
+  }px)`
+})
+
+/* -------------------------------------------------------------------------- */
+/*                   mousemove와 resize 이벤트 부하 제거하는 방법                                    */
+/* -------------------------------------------------------------------------- */
+// 1. Throttle
+// 2. Debounce
+
+function debounce(callback, limit = 100) {
+  let timeout
+  return function (...args) {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      callback.apply(this, args)
+    }, limit)
+  }
+}
+
+function throttle(callback, limit = 100) {
+  let waiting = false
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments)
+      waiting = true
+      setTimeout(() => {
+        waiting = false
+      }, limit)
+    }
+  }
+}
+
+// resize
+ground.addEventListener(
+  'mousemove',
+  debounce((e) => {
+    console.log(e.offsetX, e.offsetY)
+
+    let posX = e.offsetX
+    let posY = e.offsetY
+
+    const emotion = /* html */ `<div class="emotion" style="left:${posX}px;top:${posY}px">😘</div>`
+
+    insertLast(ground, emotion)
+  }, 1000),
+)
+
+// ground.addEventListener('mousemove', function (e) {
+//   console.log(e.offsetX, e.offsetY)
+// })
