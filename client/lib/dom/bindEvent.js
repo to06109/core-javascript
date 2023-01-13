d1ca9addb62989fde6b765e934e2d1db8e355525
to06109 <to06109@naver.com>
2023-01-13 13:57:35 +0900

    [추가] 모듈 프로그래밍

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -1,17 +1,20 @@
-function bindEvent(node, type, handler) {
-  if (typeof node === 'string') {
-    node = getNode(node)
-  }
-  // 이벤트 타입 체크
-  if (!/mouseenter|click|mousemove|mouseleave/g.test(type)) {
-    typeError(
-      'bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다.',
-    )
-  }
-  node.addEventListener(type, handler)
 
-  // 함수가 함수를 내뱉는다? 클로저
-  // bindEvent 함수를 실행하면 addEventListener를 하고
-  // 받은 node, type, handle 환경을 모두 기억하고 있는 이벤트를 지우는 함수를 내뱉는다.
-  return () => node.removeEventListener(type, handler)
+import { getNode } from "./getNode.js";
+import { typeError } from "../error/index.js";
+
+
+export function bindEvent(node,type,handler){
+  if(typeof node === 'string'){
+    node = getNode(node);
+  }
+
+  if(!(/mouseenter|click|mousemove|mouseleave/g.test(type))){
+    typeError('bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다.')
+  }
+
+
+  node.addEventListener(type,handler);
+  
+  return ()=> node.removeEventListener(type,handler);
+
 }
--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,16 +1,22 @@
-// 유틸함수 만들기 getNode에 대상 넣으면 DOM에서 찾아서 return 해주는 함수
-function getNode(node) {
-  if (typeof node !== 'string') {
-    throw new Error('getNode 함수의 인자는 문자 타입 이어야 합니다.')
+
+export function getNode(node){
+  if(typeof node !== 'string'){
+    throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
   }
 
+  // if(!isString(node)) typeError('에러가 발생했습니다.');
+  
   return document.querySelector(node)
 }
 
-function getNodes(node) {
-  if (typeof node !== 'string') {
-    throw new Error('getNode 함수의 인자는 문자 타입 이어야 합니다.')
-  }
 
+function getNodes(node){
+  if(typeof node !== 'string'){
+    throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
+  }
+  
   return document.querySelectorAll(node)
 }
+
+
+
