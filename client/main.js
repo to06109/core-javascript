78b42303d41ce6694a1a915a3794cffe45fb7abc
to06109 <to06109@naver.com>
2023-01-16 10:48:05 +0900

    [추가] diceAnimation

--- client/main.js
+++ client/main.js
@@ -1,2 +1,18 @@
-import { diceAnimation } from './lib/index.js'
+import { diceAnimation, getNode } from './lib/index.js'
 
+/* 
+  [주사위 굴리기]
+
+  1. dice 애니메이션 불러오기
+  2. bindEvent 유틸함수 만들기
+  3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
+  4. 변수 보호를 위한 클로저 + IIFE 사용하기 
+*/
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)')
+
+const handlerRollingDice = () => {
+  diceAnimation()
+}
+
+rollingDiceButton.addEventListener('click', handlerRollingDice)
