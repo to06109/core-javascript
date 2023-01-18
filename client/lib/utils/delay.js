dfc45382346308d645a2907932114dd1cfca8135
to06109 <to06109@naver.com>
2023-01-18 09:57:59 +0900

    [추가] callback 지옥 탈출하기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -6,15 +6,36 @@ function delay(callback, timeout = 1000) {
   setTimeout(callback, timeout)
 }
 
-delay(() => {
-  first.style.top = '-100px'
-  delay(() => {
-    first.style.transform = 'rotate(360deg)'
-    delay(() => {
-      first.style.top = '0px'
-    })
+// delay(() => {
+//   first.style.top = '-100px'
+//   delay(() => {
+//     first.style.transform = 'rotate(360deg)'
+//     delay(() => {
+//       first.style.top = '0px'
+//     })
+//   })
+// })
+
+delayP()
+  .then(() => {
+    first.style.top = '-100px'
+    return delayP()
   })
-})
+  .then(() => {
+    first.style.transform = 'rotate(360deg)'
+    return delayP()
+  })
+  .then(() => {
+    first.style.top = '0px'
+  })
 
+function delayP(timeout = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('성공!')
+      // reject('실패')
+    }, timeout)
+  })
+}
 
-console.log(first)
+console.log(delayP())
