ec20398a2b7e2c1d60d644a9b46f33f3e71825cf
to06109 <to06109@naver.com>
2023-01-19 15:30:24 +0900

    [추가] saveStorage함수 만들기

--- client/lib/utils/storage.js
+++ client/lib/utils/storage.js
@@ -1,7 +1,64 @@
+import { isString } from './typeOf.js'
+
 // 객체 구조 분해 할당
 const {
   localStorage: storage, // localStorage를 storage로 가져오겠다
   JSON: { stringify: serialize, parse: deserialize },
 } = globalThis
 
-console.log(localStorage)
+const albums = [
+  {
+    id: 'album_0zie',
+    title: 'Nightmare',
+    artist: '오월오일 ( 五月五日 )',
+    'release-date': '2022.10.08',
+    like: 147,
+    'song-count': 5,
+    cover: {
+      size: 640,
+      quality: 100,
+      src:
+        'https://cdnimg.melon.co.kr/cm2/album/images/110/73/494/11073494_20221007160706_500.jpg/melon/resize/640/quality/100/optimize',
+    },
+  },
+  {
+    id: 'album_9ipw',
+    title: '흔들리지 않아 (Feat. 폴킴)',
+    artist: 'TRADE L',
+    'release-date': '2022.10.07',
+    like: 306,
+    'song-count': 1,
+    cover: {
+      size: 640,
+      quality: 100,
+      src:
+        'https://cdnimg.melon.co.kr/cm2/album/images/110/72/305/11072305_20221006135934_500.jpg/melon/resize/640/quality/100/optimize',
+    },
+  },
+]
+
+// JSON.stringify()
+// serialize()
+// deserialize()
+function saveStorage(key, value) {
+  return new Promise((resolve, reject) => {
+    if (isString(key)) {
+      storage.setItem(key, value)
+      resolve()
+    } else {
+      reject({ message: 'key는 문자타입이어야 합니다.' })
+    }
+  })
+}
+
+saveStorage('name', 'tiger')
+
+/* 
+  연습
+  storage.setItem('name', 'song')
+
+  console.log(storage.getItem('name')) // song
+
+  storage.removeItem('name') // 로컬 스토리지에서 삭제됨
+  // storage.clear() // 로컬 스토리지의 모든 걸 날림 
+*/
